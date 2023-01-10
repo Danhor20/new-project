f7e6fae4023656834d69db18f158ec4aa8cb6e8d
Daniil Khoroshaev <danil.xoroshaev@mail.ru>
2023-01-10 23:01:18 +0300

    new value1

--- newProject/src/Main.java
+++ newProject/src/Main.java
@@ -1,9 +1,28 @@
 public class Main {
-
-    int a =10;
-    int b =12;
-    int c = 14;
     public static void main(String[] args) {
 
+        int a = 10;
+        int b = 12;
+        int c = 14;
+        if (a > b && a > c) {
+            System.out.printf("a больше всех");
+        } else {
+            System.out.printf("с среднее ");
+            System.out.printf("b меньше всех");
+
+        }
+
+        if (b > a && b > c) {
+            System.out.printf("b больше всех");
+        }else {
+            System.out.printf("с больше всех");
+            System.out.printf("а меньше всех");
+        }
+        if (c > a && c >b) {
+            System.out.printf("c больше всех");
+        }else {
+            System.out.printf("a меньше всех");
+            System.out.printf(" b среднее");
+        }
     }
 }
