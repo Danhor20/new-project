4987095870ffd9ef7b996212c0e12eb445b22629
Daniil Khoroshaev <danil.xoroshaev@mail.ru>
2023-01-11 19:33:13 +0300

    int new

--- newProject/src/Main.java
+++ newProject/src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int katya = 10;
-        int misha = 12;
-        int danil = 14;
+        int katyaAge = 10;
+        int mishaAge = 12;
+        int danilAge = 14;
+
+        int min = -1; // минимальный возраст
+        int middle = -1; // средний возраст
+        int max = -1; // максимальный возраст
 
 
-        if (katya > misha && katya > danil) {
+
+        if (katyaAge > mishaAge && katyaAge > danilAge) {
             System.out.printf("Misha больше всех");
         } else {
             System.out.printf("Danil среднее ");
@@ -14,19 +19,19 @@ public class Main {
 
         }
 
-        if (misha > katya && misha > danil) {
+        if (mishaAge > katyaAge && mishaAge > danilAge) {
             System.out.printf("Misha больше всех");
         }else {
             System.out.printf("Danil больше всех");
             System.out.printf("Katya меньше всех");
         }
-        if (danil > katya && danil >misha) {
+        if (danilAge > katyaAge && danilAge >mishaAge) {
             System.out.printf("Danil больше всех");
-        } if (danil > katya && misha > katya) {
+        } if (danilAge > katyaAge && mishaAge > katyaAge) {
             System.out.printf("Katya меньше всех");
             System.out.printf(" Misha среднее");
         }
 
-            
+
         }
     }
